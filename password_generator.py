1d43b3c60ef54cabe779f217dc1d1df1ac46f014
shekhshahedjiban-crypto <shekhshahedjiban@gmail.com>
2026-07-10 11:01:39 +0600

    changed the code into simple loop form

--- password_generator.py
+++ password_generator.py
@@ -2,9 +2,9 @@ import secrets #secrets module is imported to generate random secure passwords
 import string #string module for the string constants like ascii_letters and digits
 
 # Combine letters and numbers into a single pool
-pool = string.ascii_letters + string.digits #pool variable is created for the combuination of letters and numbers to generate a secure password
-
-# Securely generate a 12-character random password
-password = "".join(secrets.choice(pool) for _ in range(12))#it is used for generating a random password of length 12 by selecting random characters from the pool of letters and numbers
+pool = string.ascii_letters + string.digits #pool variable is created for the combination of letters and numbers to generate a secure password
+password ="" #password variable is initialized as an empty string to store the generated password
+for i in range(12): #loop is used to generate a password of length 12 characters
+    password += secrets.choice(pool) #secrets.choice() is used to select a random character from the pool and add it to the password variable
 
 print("Your password is:", password) #print the generated password as output to user
