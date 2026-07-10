6e2a546cfa85be85d5368c5985906047b0c8c8d6
shekhshahedjiban-crypto <shekhshahedjiban@gmail.com>
2026-07-10 10:44:47 +0600

    removed extra unwanted function

--- unit_converter.py
+++ unit_converter.py
@@ -11,15 +11,15 @@ if __name__ == "__main__":
    
     
     # taking user input for the choice of conversion
-    choice = input("Enter your choice (1 or 2): ").strip() #strip() functions is for the removal of leading and trailing whitespace characters from the input string.
+    choice = input("Enter your choice (1 or 2): ")
     
     if choice == "1": 
         try: #try block is used for handling input errors like value errors when the user enters a non numeric value for kilometers or celcius
             #taking Kilometers input from the user
             km_value = float(input("Enter distance in kilometers: "))
             miles_result = km_to_miles(km_value)
-            print(f" {km_value} km is equal to {miles_result:.2f} miles")
-        except ValueError: #if a value error occurs,the except block will be executed and will give errpr message 
+            print(f" {km_value} km is equal to {miles_result} miles")
+        except ValueError: #if a value error occurs,the except block will be executed and will give error message 
             print(" Invalid input! Please enter a valid number for kilometers.")
             
     elif choice == "2":
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             #taking Celsius input from the user
             c_value = float(input("Enter temperature in Celsius: "))
             f_result = celsius_to_fahrenheit(c_value)
-            print(f" {c_value}°C is equal to {f_result:.2f}°F")#.2f is used to format the output to 2 decimal places
+            print(f" {c_value}°C is equal to {f_result}°F")
         except ValueError:
             print(" Invalid input! Please enter a valid number for temperature.")
             
